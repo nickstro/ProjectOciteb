67ad038dcbce338bc5ef2b2212fc1d2de1773935
DanielAnonymousAlfa <colado182@gmail.com>
2019-10-22 07:12:19 -0500

    Agregado de lectura de datos de la grafica combinada

--- src/app/components/graphics/mixed/mixed.component.ts
+++ src/app/components/graphics/mixed/mixed.component.ts
@@ -10,65 +10,84 @@ am4core.useTheme(am4themes_animated);
   styleUrls: ['./mixed.component.css']
 })
 export class MixedComponent implements OnInit {
-
+  public data;
   private chart: am4charts.XYChart;
   constructor(private zone: NgZone) { }
- 
-
+  dataCopy = [];
+  dataOrder = [];
+  dataKeys = [];
   ngOnInit() {
+    this.data.sort((a, b) => a.Anio - b.Anio);
+    // @ts-ignore
+    for (var element of res) {
+      let obj = this.dataOrder.find(e =>
+        e.Anio === element.Anio
+      );
+      if (!obj) {
+        obj = { Anio: element.Anio };
+        this.dataOrder.push(obj);
+      }
+      obj[element.Tipo] = element.Total;
+    }
+
+    for (var a of this.dataOrder) {
+      this.dataKeys.push(a);
+    }
+
+    var aux = this.dataKeys;
+    for (var i of aux) {
+      if (aux.length = 1) {
+      } else {
+        aux.pop();
+      }
+    }
+    for (var e of aux) {
+      this.dataCopy.push(Object.keys(e));
+    }
+    this.createGraphic();
+  }
+
+  createGraphic() {
     this.zone.runOutsideAngular(() => {
-      let chart = am4core.create("chartdiv2", am4charts.XYChart);
-      chart.data = [{
-        "year": "2015",
-        "income": 23.5,
-        "expenses": 18.1
-    }, {
-        "year": "2016",
-        "income": 26.2,
-        "expenses": 22.8
-    }];
+      let chart = am4core.create("chartdiv", am4charts.XYChart);
+      chart.data = this.dataOrder;
+      let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "Anio";
+      categoryAxis.renderer.inversed = true;
+      categoryAxis.renderer.grid.template.location = 0;
 
-//create category axis for years
-var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "year";
-categoryAxis.renderer.inversed = true;
-categoryAxis.renderer.grid.template.location = 0;
+      function createSeries(info1, info2, info3) {
+      let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+      valueAxis.renderer.opposite = true;
+      let series = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.categoryY = info1;
+      series.dataFields.valueX = info2;
+      series.name = info2;
+      series.columns.template.fillOpacity = 0.5;
+      series.columns.template.strokeOpacity = 0;
+      series.tooltipText = "Resultados {categoryY}: {valueX.value}";
+      let lineSeries = chart.series.push(new am4charts.LineSeries());
+      lineSeries.dataFields.categoryY = info1;
+      lineSeries.dataFields.valueX = info2;
+      lineSeries.name = info2;
+      lineSeries.strokeWidth = 3;
+      lineSeries.tooltipText = "Datos {categoryY}: {valueX.value}";
+      let circleBullet = lineSeries.bullets.push(new am4charts.CircleBullet());
+      circleBullet.circle.fill = am4core.color("#fff");
+      circleBullet.circle.strokeWidth = 2;
+      return series;
+    }
 
-//create value axis for income and expenses
-var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-valueAxis.renderer.opposite = true;
+    for (var j of this.dataCopy) {
+      createSeries("Anio", j[2],j[3]);
+    }
 
+      //add chart cursor
+      chart.cursor = new am4charts.XYCursor();
+      chart.cursor.behavior = "zoomY";
 
-//create columns
-var series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.categoryY = "year";
-series.dataFields.valueX = "income";
-series.name = "Income";
-series.columns.template.fillOpacity = 0.5;
-series.columns.template.strokeOpacity = 0;
-series.tooltipText = "Income in {categoryY}: {valueX.value}";
-
-//create line
-var lineSeries = chart.series.push(new am4charts.LineSeries());
-lineSeries.dataFields.categoryY = "year";
-lineSeries.dataFields.valueX = "expenses";
-lineSeries.name = "Expenses";
-lineSeries.strokeWidth = 3;
-lineSeries.tooltipText = "Expenses in {categoryY}: {valueX.value}";
-
-//add bullets
-var circleBullet = lineSeries.bullets.push(new am4charts.CircleBullet());
-circleBullet.circle.fill = am4core.color("#fff");
-circleBullet.circle.strokeWidth = 2;
-
-//add chart cursor
-chart.cursor = new am4charts.XYCursor();
-chart.cursor.behavior = "zoomY";
-
-//add legend
-chart.legend = new am4charts.Legend();
-  });
-
-
+      //add legend
+      chart.legend = new am4charts.Legend();
+    });
   }
 }
